1244923f647ebcea74f439bfd654c3647073c064
Kevin Miller <kemiller2002@gmail.com>
2022-01-09 12:46:16 -0500

    setup

--- wordpress-converter/src/index.ts
+++ wordpress-converter/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const fs = require("fs");
+const parser = require("xml2json");
+
+const file = "/downloads/structuredsight.WordPress.2021-12-24.xml";
+
+fs.readFile(file, function (err: Error, data: string) {
+  var json = parser.toJson(data);
+  console.log("to json ->", json);
+});
